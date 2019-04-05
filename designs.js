7ee29c8da82a9282ec4773f0e4bea82ec1641b43
Nicky Fusté <nicky.fuste@gmail.com>
2019-04-05 15:17:17 +0200

    First approach

--- designs.js
+++ designs.js
@@ -1,10 +1,41 @@
 // Select color input
+let colorInput = document.querySelector("#colorPicker");
+
 // Select size input
+let sizeInput = document.querySelector("#sizePicker");
+let inputHeight = document.querySelector("#inputHeight");
+let inputWidth = document.querySelector("#inputWidth");
 
 // When size is submitted by the user, call makeGrid()
+sizeInput.addEventListener("submit", makeGrid);
 
-function makeGrid() {
+function makeGrid(event) {
+    console.log("Work in progress");
+    event.preventDefault();
+    let table = document.querySelector("#pixelCanvas");
+    let tableBody = document.createElement("tbody");
+    let height = inputHeight.value;
+    let width = inputWidth.value;
 
-// Your code goes here!
+    // Create table rows
+    for (let i = 0; i < height; i++) {
+        // Create one row
+        let row = document.createElement("tr");
 
+        for (let j = 0; j < width; j++) {
+            // Create one column
+            let column = document.createElement("td");
+            column.addEventListener("click", changeColor);
+            row.appendChild(column);
+        }
+        tableBody.appendChild(row);
+    }
+
+    table.appendChild(tableBody);
+
+    
+}
+
+function changeColor() {
+    console.log("hola");
 }
